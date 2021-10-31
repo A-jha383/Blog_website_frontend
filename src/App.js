170bc5b0f568e22396dd9ee7aa1798853994a51b
A-jha383 <ayushj383@gmail.com>
2021-10-31 17:32:16 +0530

    pages added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticleList from './pages/ArticleList';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page-body">
+    <Router>
+       <Route path="/" component={HomePage} exact />
+       <Route path="/about" component={AboutPage}/>
+       <Route path="/articles-list" component={ArticleList}/>
+       <Route path="/article" component={ArticlePage}/>
+    </Router>
     </div>
   );
 }
