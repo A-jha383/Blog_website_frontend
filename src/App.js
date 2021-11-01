35799d48cf604dfe717aa5dcc75364fde8370498
A-jha383 <ayushj383@gmail.com>
2021-11-01 12:26:16 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
-import './App.css';
+  Route,
+} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticleList from './pages/ArticleList';
 import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
+import './App.css';
 
-function App() {
-  return (
-    <div id="page-body">
-    <Router>
-       <Route path="/" component={HomePage} exact />
-       <Route path="/about" component={AboutPage}/>
-       <Route path="/articles-list" component={ArticleList}/>
-       <Route path="/article" component={ArticlePage}/>
-    </Router>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div id="page-body">
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/articles-list" component={ArticleList} />
+            <Route path="/article/:name" component={ArticlePage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
